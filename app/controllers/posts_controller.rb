43664123bf48332e9708fe0ac8a255d89df37697
qianjing00 <mufc12159@gmail.com>
2022-01-17 17:47:39 +0900

    create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.new
     @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:kibun)
   end
 end

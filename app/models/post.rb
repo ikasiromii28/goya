c8a005933494ae50411bd4f224e1334bc3a276c2
qianjing00 <mufc12159@gmail.com>
2022-01-18 16:13:12 +0900

    update posts#index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  # # validates :kibun, presence: true
+  # validates :date, presence: true
 end
--- db/migrate/20220117075204_create_posts.rb
+++ db/migrate/20220117075204_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.string :kibun, null: false
+      t.date :date, null: false
       t.timestamps
     end
   end

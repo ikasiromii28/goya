8849fa57f42d39a42959fd601e1fbdd66291d00d
qianjing00 <mufc12159@gmail.com>
2022-01-24 19:42:05 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,17 @@ class Post < ApplicationRecord
   validates :date, presence: true
 
   belongs_to :user
+
+  def self.sort(selection)
+    case selection
+    when 'new'
+      all.order(created_at: :DESC)
+    when 'old'
+      all.order(created_at: :ASC)
+    when 'date-new'
+      all.order(date: :DESC)
+    when 'date-old'
+      all.order(date: :ASC)
+    end
+  end
 end
